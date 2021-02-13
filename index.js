63a7a746bcb2559513be07ede6b5674be9a1725a
Jordan Mossing <jordan.mossing@gmail.com>
2021-02-13 11:19:53 -0500

    adding updated html css and js files

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+// Make sure that we grabbing the references to the HTML elements
+
+
+// Create a function to test USER Input capture (Data submitted by form - City, Location)
+
+
+
+// Info ('CITY') from User --> Make an API call with that information (Async Function Call --> )
+
+
+    // -- Wait for data to come back (.then() --> PROMISE HANDLING ) ( and do what? )
+
+        // -- Parse the Data 
+
+        // Update the Information on the DOM
+
+
+    
+//  -- How to capture the City request?
+//  -- How to Make API Request for respective data (Current Weather vs. Forcast Weather vs. whatverer) 
+//   -- Dealing with Asynchronous Calls 
